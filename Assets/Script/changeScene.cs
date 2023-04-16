682ed92d35122a7af9f044b8d16204be3807e7ba
Naitik Chovatiya <naitikchovatiya2021@gmail.com>
2023-04-15 22:37:29 -0400

    Final work done

---
+++ Assets/Script/changeScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class changeScene : MonoBehaviour
+{
+    public void loadScene(string scene)
+    {
+    SceneManager.LoadScene(scene);
+    }
+
+    public void GoToMainMenu()
+    {
+    SceneManager.LoadScene("Main Menu");
+    }
+}
+
